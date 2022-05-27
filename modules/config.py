90130b529787b24f0cd07aa389745fc4a32b91a2
Hell-x <106371589+Hell-x@users.noreply.github.com>
2022-05-27 17:57:12 +0530

    Add files via upload

---
+++ modules/config.py
@@ -0,0 +1,33 @@
+# 𝐌𝐨𝐝𝐮𝐥𝐞𝐬 𝐚𝐧𝐝 𝐄𝐧𝐯𝐢𝐫𝐨𝐧𝐦𝐞𝐧𝐭𝐬
+import os
+import aiohttp
+from os import getenv
+from dotenv import load_dotenv
+
+# 𝐈𝐧𝐭𝐞𝐫𝐧𝐚𝐥 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 (@HELL_X_MUSIC_ASSISTANT)
+load_dotenv()
+que = {}
+admins = {}
+aiohttpsession = aiohttp.ClientSession()
+
+# 𝐑𝐞𝐪𝐮𝐢𝐫𝐞𝐝 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 //𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 @HELL_X_EMPIRE
+API_HASH = getenv("API_HASH", "XXXXX")
+API_ID = int(getenv("API_ID", "XXXXX"))
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "XXXXX")
+ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME", "XXXXX")
+BOT_IMAGE = getenv("BOT_IMAGE", "https://te.legra.ph/file/8fd190dce43663f755088.jpg")
+BOT_NAME = getenv("BOT_NAME", "XXXXX")
+BOT_TOKEN = getenv("BOT_TOKEN", "12345:XXXXX")
+BOT_USERNAME = getenv("BOT_USERNAME", "XXXXX")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
+OWNER_NAME = getenv("OWNER_NAME", "HELL HANDLER")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "AdityaHalder")
+SOURCE_CODE = getenv("SOURCE_CODE", "https://github.com/Hell-x/HELLXMUSIC")
+STRING_SESSION = getenv("STRING_SESSION", "session")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/SCHOOL_WALLI_MASTII")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "https://t.me/HELL_X_EMPIRE")
+
+# 𝐃𝐨 𝐍𝐨𝐭 𝐂𝐡𝐚𝐧𝐠𝐞 𝐓𝐡𝐢𝐬 𝐋𝐢𝐧𝐞𝐬 // 𝐈𝐠𝐧𝐨𝐫𝐞 𝐓𝐡𝐢𝐬 (𝐀𝐝𝐢𝐭𝐲𝐚 𝐇𝐚𝐥𝐝𝐞𝐫) 
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+PROFILE_CHANNEL = getenv("PROFILE_CHANNEL", "https://t.me/MAMBA_NETWORK")
